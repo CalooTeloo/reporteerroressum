1dd4647e49224a9ab1f04bc6f82bfab6030d2357
calichitello0 <calichitello0@gmail.com>
2025-03-13 04:10:53 +0000

    commit

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,8 +2,8 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.example.app',
-  appName: 'RepoTelo',
-  webDir: 'dist'
+  appName: 'myapp',
+  webDir: 'src'
 };
 
 export default config;
